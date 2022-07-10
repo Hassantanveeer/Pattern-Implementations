51f9d4410729ee0cfe0397407026868779b1ed67
Veena095 <108769132+Veena095@users.noreply.github.com>
2022-07-10 12:34:11 -0500

    Implementing MouseListener

---
+++ src/controller/MouseListener.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import controller.event.EventFactory;
+import controller.interfaces.IMouseEvent;
+import model.Point;
+import model.persistence.ApplicationState;
+import view.interfaces.PaintCanvasBase;
+
+public class MouseListener extends MouseAdapter{
+
+    private Point startPoint;
+    private PaintCanvasBase paintCanvas;
+    private ApplicationState appState;
+
+    public MouseListener(PaintCanvasBase baseCanvas, ApplicationState baseState) {
+        paintCanvas = baseCanvas;
+        appState = baseState;
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        startPoint = new Point(e.getX(), e.getY());
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        Point endPoint = new Point(e.getX(), e.getY());
+        IMouseEvent newEvent = EventFactory.getEvent(startPoint, endPoint, paintCanvas, appState);
+        newEvent.Execute();
+    }
+}

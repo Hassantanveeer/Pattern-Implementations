8ff82f7f337f80e4a40efc5fbadff9a9fbf6d23f
Hassan Tanveer <hassantanveeer@gmail.com>
2022-07-10 22:15:06 -0500

    First Sprint week 4
    First Check In completed

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,9 @@ package main;
 
 import controller.IJPaintController;
 import controller.JPaintController;
+import controller.MouseListener;
 import model.ShapeColor;
+import model.ShapeList;
 import model.ShapeType;
 import model.persistence.ApplicationState;
 import view.gui.Gui;
@@ -18,35 +20,16 @@ import java.util.EnumMap;
 
 public class Main {
     public static void main(String[] args){
-        PaintCanvasBase paintCanvas = new PaintCanvas();
+    	PaintCanvasBase paintCanvas = new PaintCanvas();
         IGuiWindow guiWindow = new GuiWindow(paintCanvas);
         IUiModule uiModule = new Gui(guiWindow);
         ApplicationState appState = new ApplicationState(uiModule);
         IJPaintController controller = new JPaintController(uiModule, appState);
+        ShapeList shapeList = new ShapeList(paintCanvas);
+
+        MouseListener mouseListener=new MouseListener(paintCanvas,appState,shapeList);
+        paintCanvas.addMouseListener(mouseListener);
+        
         controller.setup();
-
-        // For example purposes only; remove all lines below from your final project.
-
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Filled in rectangle
-        Graphics2D graphics2d = paintCanvas.getGraphics2D();
-        graphics2d.setColor(Color.GREEN);
-        graphics2d.fillRect(12, 13, 200, 400);
-
-        // Outlined rectangle
-        graphics2d.setStroke(new BasicStroke(5));
-        graphics2d.setColor(Color.BLUE);
-        graphics2d.drawRect(12, 13, 200, 400);
-
-        // Selected Shape
-        Stroke stroke = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1, new float[]{9}, 0);
-        graphics2d.setStroke(stroke);
-        graphics2d.setColor(Color.BLACK);
-        graphics2d.drawRect(7, 8, 210, 410);
-    }
+}
 }
---
+++ src/model/interfaces/IShape.java
@@ -0,0 +1,9 @@
+package model.interfaces;
+
+
+import java.awt.*;
+
+public interface IShape {
+    void draw(Graphics2D g);
+    void clear(Graphics2D g);
+}

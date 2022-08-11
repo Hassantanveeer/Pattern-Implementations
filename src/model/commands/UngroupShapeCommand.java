ebbfc911822ebdadb93414c463f0ea32602a1c78
Hassan Tanveer <hassantanveeer@gmail.com>
2022-08-11 14:02:36 -0500

    Started Sprint 4 added Commands

---
+++ src/model/commands/UngroupShapeCommand.java
@@ -0,0 +1,28 @@
+package model.commands;
+import model.GroupShape;
+import model.ShapeList;
+import model.interfaces.ICommand;
+
+public class UngroupShapeCommand implements ICommand {
+    ShapeList shapeList;
+    GroupShape groupShape;
+    public UngroupCommand(ShapeList shapeList) {
+        this.shapeList = shapeList;
+    }
+
+    public void run() {
+        groupShape = new GroupShape(shapeList);
+        groupShape.removeFromGroup();
+        CommandHistory.add(this);
+    }
+
+    @Override
+    public void undo() {
+        groupShape.undoRemove();
+    }
+
+    @Override
+    public void redo() {
+        groupShape.redoRemove();
+    }
+}
